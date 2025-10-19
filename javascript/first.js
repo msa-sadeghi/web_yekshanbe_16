0ef522c89544c4a300ebf4b7499d121403625359
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-19 16:42:47 +0330

    sd

---
+++ javascript/first.js
@@ -0,0 +1,6 @@
+let firstName = "nikan";
+let lastName = "ahmadi"
+console.log("this is a test")
+console.log("this is a blalal")
+console.log("hello " + firstName + " " + lastName);
+console.log(`hello ${firstName} ${lastName}`);

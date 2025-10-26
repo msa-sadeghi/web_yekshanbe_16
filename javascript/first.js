1d999e306fa751df01b8689812bfec87827cf074
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-26 16:57:47 +0330

    ss

--- javascript/first.js
+++ javascript/first.js
@@ -1,8 +1,64 @@
-let divElement = document.getElementById("welcomeMessage")
+// let divElement = document.getElementById("welcomeMessage")
 
 
-function showWelcome(){
-    let username = prompt("لطفا اسم خود را وارد نمائید")
-    divElement.textContent = `سلام ${username} چطوری؟`
-    divElement.style.display = "block"
+// function showWelcome1(){
+//     // let username = prompt("لطفا اسم خود را وارد نمائید")
+//     // let age = prompt("لطفا سن خود را وارد نمائید")
+//     // divElement.textContent = `سلام ${username}   سنت ${age}چطوری؟`
+//     divElement.textContent = "salaam"
+//     divElement.style.display = "block"
+// }
+// function showWelcome2(){
+    
+//     divElement.textContent = "hi"
+//     divElement.style.display = "block"
+// }
+// function showWelcome3(){
+//     divElement.textContent = "hello"
+//     divElement.style.display = "block"
+// }
+
+
+
+// let x1 = Number(prompt("enter the number :"))
+// let x2 = Number(prompt("enter the number :"))
+// console.log(x1 + x2)
+
+// let x3 = +prompt("enter the number :")
+// let x4 = +prompt("enter the number :")
+// console.log(x4 + x3)
+
+
+// const x  = 12
+
+
+// var y = 12
+// let z = 14
+
+// function a(){
+//     let xx = 12
+// }
+
+// console.log(xx)
+
+// if (1==1){
+//     let yy = 14
+// }
+
+// console.log(yy)
+
+// if (1==1){
+//     var yy = 14
+// }
+
+// console.log(yy)
+
+const person = {
+    name : "reza",
+    age:17
 }
+
+console.log(person.name)
+console.log(person["name"])
+
+console.log(typeof person)

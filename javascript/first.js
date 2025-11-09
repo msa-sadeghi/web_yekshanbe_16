03808d011834c6a21d88e27198081b874f538041
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-09 16:56:56 +0330

    s

--- javascript/first.js
+++ javascript/first.js
@@ -1,80 +1,60 @@
-// let x = 0
-// x = x + 1
-// x += 1
-// x++
-// console.log(x)
-// for(let i = 0; i < 3; i++){
-//     console.log(`number ${i}`)
+// for(let i = 0; i < 5; i++){
+//     console.log(`iteration number ${i}`)
+// }
+// let username = "admin"
+// for(let i = 0; i < username.length; i++){
+//     console.log(username[i])
+// }
+// let names = ["amir", "sara", "babak", "pezham"]
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i])
 // }
 
-// let counter = 0
-// while(counter < 3){
-//     console.log(`item # ${counter}`)
-//     counter++
+// let result = 0;
+// for (let i = 1; i <= 100; i++){
+//     result += i
 // }
-
-// let username = "sara"
-// console.log(username[0])
-
-// let username = 'sara'
-// console.log(username[0])
-
-// let username = `sara`
-// console.log(username[0])
-// console.log(username.length)
-// for(let j = 0; j < username.length;  j++){
-//     console.log(username[j])
+// console.log(`sum of numbers = ${result}`)
+// let result2 = 0;
+// for (let i = 1; i <= 100; i+=2){
+//     result2 += i
 // }
+// console.log(`sum of numbers = ${result2}`)
 
-// let number = 123
-// let strNumber = number.toString()
-// console.log(typeof strNumber)
 
-// let another = "123"
-// let numberAnother = Number(another)
-// console.log(typeof numberAnother)
+// let age = 19
 
-// true    false
-
+// if (age < 8){
+//     console.log("baby")
+// }else if(age >= 9 && age <=13){
+//     console.log("teenager")
+// }else{
+//     console.log("adult")
+// }
 // let person = {
-//     username: "sara",
-//     password : "12345"
+//     name : "ali",
+//     age:23
 // }
-// console.log(person.username)
-// console.log(person.password)
-// console.log(person['username'])
-// console.log(person['password'])
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person["name"])
+// console.log(person["age"])
+// let x = 123
+// let y = x.toString()
+// let z = String(x)
 
-// let x  = 12
-// let y = "12"
-// console.log(x === y)
+// console.log(123 === "123")
 
-// //   and    or      &&     ||
+ let names = ["amir", "sara", "babak", "pezham"]
 
-// let  numbers = [1, 2, 3, 4, 5]
-// let sum = 0
-// numbers.forEach(function(n){
-//     console.log(n)
-//     sum += n
-// })
-// console.log(sum)
+ names.forEach(function(n){
+    console.log(n)
+ })
 
-let tempInput = document.getElementById("tempInput")
-let tempFrom = document.getElementById("tempFrom").value
-let tempTo = document.getElementById("tempTo").value
-let tempResult = document.getElementById("tempResult")
+ let numbers = [1,2,3,4,5,6,7,8,9]
+ let result = 0
+ numbers.forEach(function(x){
+    result += x
+ })
 
-function convertTemperature(){
-    let val = tempInput.value
-    let result
-    if(tempFrom ==  tempTo){
-        result = val
-    }else if (tempFrom === "celsius"){
-        if (tempTo === "fahrenheit"){
-            result = val  *2 
-        }
-    }
-    console.log(tempFrom, tempTo)
-    tempResult.textContent = result
-    tempResult.style.display = "block"
-}
+ console.log(result)

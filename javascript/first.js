2623440389a9c8b0ffa805707f16abcaf59e9844
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-16 16:15:57 +0330

    s

--- javascript/first.js
+++ javascript/first.js
@@ -1,60 +1,34 @@
-// for(let i = 0; i < 5; i++){
-//     console.log(`iteration number ${i}`)
-// }
-// let username = "admin"
-// for(let i = 0; i < username.length; i++){
-//     console.log(username[i])
-// }
-// let names = ["amir", "sara", "babak", "pezham"]
-// for(let i = 0; i < names.length; i++){
-//     console.log(names[i])
-// }
-
-// let result = 0;
-// for (let i = 1; i <= 100; i++){
-//     result += i
-// }
-// console.log(`sum of numbers = ${result}`)
-// let result2 = 0;
-// for (let i = 1; i <= 100; i+=2){
-//     result2 += i
-// }
-// console.log(`sum of numbers = ${result2}`)
 
 
-// let age = 19
+function convertTemperature(){
+   let tempInput = document.getElementById("tempInput").value
+   let tempFrom = document.getElementById("tempFrom").value
+   let tempTo = document.getElementById("tempTo").value
+   let tempResult = document.getElementById("tempResult")
 
-// if (age < 8){
-//     console.log("baby")
-// }else if(age >= 9 && age <=13){
-//     console.log("teenager")
-// }else{
-//     console.log("adult")
-// }
-// let person = {
-//     name : "ali",
-//     age:23
-// }
-// console.log(person.name)
-// console.log(person.age)
-// console.log(person["name"])
-// console.log(person["age"])
-// let x = 123
-// let y = x.toString()
-// let z = String(x)
+   let result
 
-// console.log(123 === "123")
-
- let names = ["amir", "sara", "babak", "pezham"]
-
- names.forEach(function(n){
-    console.log(n)
- })
-
- let numbers = [1,2,3,4,5,6,7,8,9]
- let result = 0
- numbers.forEach(function(x){
-    result += x
- })
-
- console.log(result)
+   if(tempFrom == tempTo){
+      result = tempInput
+   }else if(tempFrom == "celsius"){
+      if(tempTo == "fahrenheit"){
+         result = tempInput * 2
+      }else if(tempTo == "kelvin"){
+         result = tempInput * 3
+      }
+   }else if(tempFrom == "fahrenheit"){
+      if(tempTo == "celsius"){
+         result = tempInput * 2
+      }else if(tempTo == "kelvin"){
+         result = tempInput * 3
+      }
+   }else if(tempFrom == "kelvin"){
+      if(tempTo == "celsius"){
+         result = tempInput * 2
+      }else if(tempTo == "fahrenheit"){
+         result = tempInput * 3
+      }
+   }
+   tempResult.innerHTML = result
+   tempResult.style.display = "block"
+}

a8beaae9025a93f0f0bed6ee4658a032f8c067d2
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-16 16:38:00 +0330

    s

---
+++ javascript/app.js
@@ -0,0 +1,66 @@
+// let score = 0
+// console.log(score)
+// score = score + 1
+// console.log(score)
+// score += 1
+// console.log(score)
+// score++
+// console.log(score)
+
+// console.log("123" == 123)
+// console.log("123" === 123)
+
+// &&     ||    !
+
+// let age = 17
+
+// if(age >= 15 && age <= 23){
+//     console.log("blalalal")
+// }
+// if(age >= 15 || age <= 23){
+//     console.log("blalalal")
+// }
+
+// let isLoggedIn = false
+// if(!isLoggedIn){
+//     console.log("yes")
+// }
+
+// let isLoggedIn = true
+// if(isLoggedIn){
+//     document.body.innerHTML = "<h1>Welcome</h1>"
+// }else{
+//     document.body.innerHTML = "<h1>Not LoggedIn</h1>"
+// }
+
+// isLoggedIn ?  document.body.innerHTML =
+//  "<h1>Welcome</h1>" : document.body.innerHTML =
+//   "<h1>Not LoggedIn</h1>"
+
+let dayNumber = 1
+switch(dayNumber){
+    case 1:
+        console.log("sat")
+        break
+    case 2:
+        console.log("sun")
+        break
+    case 3:
+        console.log("mon")
+        break
+    case 4:
+        console.log("tue")
+        break
+    case 5:
+        console.log("wed")
+        break
+    case 6:
+        console.log("thurs")
+        break
+    case 7:
+        console.log("fri")
+        break
+    default:
+        console.log("not valid number")
+
+}

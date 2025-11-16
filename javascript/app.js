a58460c1626936af82553e117168a4ed65ef4694
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-16 16:57:28 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -37,30 +37,69 @@
 //  "<h1>Welcome</h1>" : document.body.innerHTML =
 //   "<h1>Not LoggedIn</h1>"
 
-let dayNumber = 1
-switch(dayNumber){
-    case 1:
-        console.log("sat")
-        break
-    case 2:
-        console.log("sun")
-        break
-    case 3:
-        console.log("mon")
-        break
-    case 4:
-        console.log("tue")
-        break
-    case 5:
-        console.log("wed")
-        break
-    case 6:
-        console.log("thurs")
-        break
-    case 7:
-        console.log("fri")
-        break
-    default:
-        console.log("not valid number")
+// let dayNumber = 1
+// switch(dayNumber){
+//     case 1:
+//         console.log("sat")
+//         break
+//     case 2:
+//         console.log("sun")
+//         break
+//     case 3:
+//         console.log("mon")
+//         break
+//     case 4:
+//         console.log("tue")
+//         break
+//     case 5:
+//         console.log("wed")
+//         break
+//     case 6:
+//         console.log("thurs")
+//         break
+//     case 7:
+//         console.log("fri")
+//         break
+//     default:
+//         console.log("not valid number")
 
-}
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9]
+// let s = 0
+// for(let i = 0; i < numbers.length; i++){
+//     s += numbers[i]
+// }
+// console.log(s)
+// s  = 0
+// numbers.forEach(function(n){
+//     s += n
+// })
+// console.log(s)
+
+// let i =  0
+// s = 0
+// while(i < numbers.length){
+//     s += numbers[i]
+//     i++
+// }
+// console.log(s)
+
+// s = 0
+// i = 0
+// do{
+//     s += numbers[i]
+//     i++
+// }while(i < numbers.length)
+// console.log(s)
+
+let items = ["sport", "food", "jobs", "math", "algebra"]
+
+let result = "<ul>"
+
+items.forEach(function(x){
+    result += "<li>" + x + "</li>"
+})
+result += "</ul>"
+
+document.body.innerHTML = result

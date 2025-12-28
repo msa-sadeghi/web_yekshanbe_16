4527bc344c47cc93c2f5dc77680c4e5bf40fdd9d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-28 16:09:06 +0330

    s

--- javascript/first.js
+++ javascript/first.js
@@ -1,34 +1,13 @@
+const btn = document.getElementsByTagName('button')[0]
+const p = document.querySelector("p")
+const img = document.querySelector('img')
+btn.addEventListener('click', () => {
+    // p.classList.add("red")
+    p.classList.toggle("red")
+    if(img.getAttribute("src") === ""){
 
-
-function convertTemperature(){
-   let tempInput = document.getElementById("tempInput").value
-   let tempFrom = document.getElementById("tempFrom").value
-   let tempTo = document.getElementById("tempTo").value
-   let tempResult = document.getElementById("tempResult")
-
-   let result
-
-   if(tempFrom == tempTo){
-      result = tempInput
-   }else if(tempFrom == "celsius"){
-      if(tempTo == "fahrenheit"){
-         result = tempInput * 2
-      }else if(tempTo == "kelvin"){
-         result = tempInput * 3
-      }
-   }else if(tempFrom == "fahrenheit"){
-      if(tempTo == "celsius"){
-         result = tempInput * 2
-      }else if(tempTo == "kelvin"){
-         result = tempInput * 3
-      }
-   }else if(tempFrom == "kelvin"){
-      if(tempTo == "celsius"){
-         result = tempInput * 2
-      }else if(tempTo == "fahrenheit"){
-         result = tempInput * 3
-      }
-   }
-   tempResult.innerHTML = result
-   tempResult.style.display = "block"
-}
+        img.src = "bmw.png"
+    }else{
+        img.src = ""
+    }
+})

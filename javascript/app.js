555b2cd04ca88fc3430a6b882638d126bf75c719
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-01-04 16:11:47 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,17 @@
-let body = document.body
+let todoInput = document.getElementById("todoInput")
+let todoList = document.getElementById("todoList")
 
-function toggleTheme(){
-    body.classList.toggle("dark")
-    }
+function addTodo(){
+    let newLi = document.createElement("li")
+    newLi.className = "todo-item"
+    let newSpan = document.createElement('span')
+    newSpan.innerHTML = todoInput.value
+    let newI = document.createElement("i")
+    newI.className= "fa-regular fa-trash-can"
+    newI.addEventListener('click', (e)=>{
+        newLi.remove()
+    })
+    newLi.append(newSpan, newI)
+    todoList.append(newLi)
+    todoInput.value = ''
+}
